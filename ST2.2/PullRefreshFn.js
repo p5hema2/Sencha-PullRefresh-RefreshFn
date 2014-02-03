8a530a6a58ce345b51c468bb839f3fa9225a29d9
Martin Hess <martin.hess85@gmail.com>
2014-02-03 10:29:20 +0100

    Moved PullRefreshFn.js ST2.2

---
+++ ST2.2/PullRefreshFn.js
@@ -0,0 +1,75 @@
+/**
+ * This user extension gives st 2.2.0 Pullrefresh the RefreshFn back
+ * based on sencha touch 2.2.0 beta 1
+ * 
+ * @class Ext.ux.touch.PullRefreshFn
+ * @version 1.0.0
+ * @author Martin Hess <https://github.com/p5hema2>
+ * @author Martin Hess <https://github.com/flyacts>
+ *
+ * ## Example
+ *
+ *     Ext.create('Ext.dataview.List', {
+ *         fullscreen: true,
+ *
+ *         store: store,
+ *
+ *         plugins: [
+ *             {
+ *                 xclass: 'Ext.ux.touch.PullRefreshFn',
+ *                 pullRefreshText: 'Pull down for more new Tweets!'
+ *                 refreshFn: function() { 
+ *		                Ext.getStore('ENTER YOUR STORE HERE').load('',1)
+ *		           }
+ *             }
+ *         ],
+ *
+ *         itemTpl: [
+ *             'YOUR ITEMTPL'
+ *         ]
+ *     });
+ */
+Ext.define('Ext.ux.touch.PullRefreshFn', {
+    extend: 'Ext.plugin.PullRefresh',
+    alias: 'plugin.pullrefreshfn',
+    requires: ['Ext.DateExtras'],
+
+    config: {
+    	/**
+         * @cfg {Function} refreshFn The function that will be called to refresh the list.
+         * If this is not defined, the store's load function will be called.
+         * The refresh function gets called with a reference to this plugin instance.
+         * @accessor
+         */
+        refreshFn: null
+    },
+
+	loadStore: function() {
+	    var me = this,
+	        list = me.getList(),
+	        scroller = list.getScrollable().getScroller();
+	
+	
+	    me.setViewState('loading');
+	    me.isReleased = false;
+	
+	    Ext.defer(function() {
+	        scroller.on({
+	            scrollend: function() {
+	                if (me.getRefreshFn()) {
+	                    me.getRefreshFn().call(me, me);
+	                } else {
+	                    me.fetchLatest();
+	                }
+	                me.resetRefreshState();
+	            },
+	            delay: 100,
+	            single: true,
+	            scope: me
+	        });
+	        scroller.minPosition.y = 0;
+	        scroller.scrollTo(null, 0, true);
+	    }, 500, me);
+	
+	}
+});
